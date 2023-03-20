5fa872385521d955b9b433ed84b96fca8290e3f3
upread <up777up@yandex.ru>
2023-03-20 17:31:14 +0300

    2023-03-20

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,22 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        <meta name="description" content="">
+        <base href="{{env('APP_URL')}}" />
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <link rel="stylesheet" href="/style.css">
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    <body>
+        <div class="">
             @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-
-            <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,12 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        <meta name="description" content="">
+        <base href="{{env('APP_URL')}}" />
         <title>{{ config('app.name', 'Laravel') }}</title>
-
+        <link rel="stylesheet" href="/style.css">
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
+        <div class="">jjj
             {{ $slot }}
         </div>
     </body>
